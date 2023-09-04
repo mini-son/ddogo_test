683ac631546971908975beca2f2af4c6c98ea687
min <happysss0505@gmail.com>
2023-09-04 21:01:32 +0900

    9/4 파라미터 넘기는 것까지 성공(select3/sigunguMap4/initialData//main/select31)

--- src/main/java/com/yumpro/ddogo/main/controller/MainController.java
+++ src/main/java/com/yumpro/ddogo/main/controller/MainController.java
@@ -93,6 +93,62 @@ public class MainController {
     }
 
 
+  
+
+
+
+    //---------------한번에 main2.html-------------------
+    //오늘 전국 베스트 찜
+    @RequestMapping("main")
+    public String main(HttpServletResponse response,Model model) throws Exception {
+        //일별 베스트
+        //맛집
+        List<HashMap<String, Object>> eatjjim = mainService.eatjjim();
+        //카페
+        List<HashMap<String, Object>> cafejjim = mainService.cafejjim();
+        System.out.println("eatjjim"+eatjjim);
+        System.out.println("cafejjim"+cafejjim);
+
+        model.addAttribute("cafejjim",cafejjim);
+        model.addAttribute("eatjjim",eatjjim);
+
+        //월별베스트
+        Map<String,List<String>> sigunguMap = mainService.getsidogungu();
+        JSONObject obj = new JSONObject();
+        response.setContentType("text/html; charset=UTF-8");
+        PrintWriter out = response.getWriter();
+
+        for(String sidoKey: sigunguMap.keySet() ) { //map의 key의 개수만큼=<=(중복되지않는)sido의 개수만큼
+            List<String> sigunguList = sigunguMap.get(sidoKey);
+            JSONArray jsonArr = new JSONArray(); //배열준비
+            System.out.printf("시도별%s 시군구개수%d \r\n",sidoKey,sigunguList.size());
+            for( String gugun :sigunguList) { //sido별  sigungu의 개수만큼
+                jsonArr.put(gugun);
+            }
+            obj.put(sidoKey,jsonArr);
+        }
+
+        String jsonStr = obj.toString();
+        System.out.println("jsonStr ="+jsonStr); //콘솔출력.확인용
+        //out.print(jsonStr); //client로 보내기
+        model.addAttribute(jsonStr);
+        model.addAttribute("sigunguMap",sigunguMap);
+        System.out.println("sigunguMap_test2"+sigunguMap);
+
+        return "main/main3";
+
+    }
+
+    @GetMapping(value = "sigunguMap3", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public Map<String, List<String>> select_ajax3(HttpServletResponse response, Model model) throws Exception {
+        Map<String, List<String>> sigunguMap = mainService.getsidogungu();
+        model.addAttribute("sigunguMap",sigunguMap);
+        System.out.println("sigunguMap"+sigunguMap);
+        return sigunguMap;
+    }
+
+
     //-----------------test-----------------
     @GetMapping(value = "select2")
     public String select_ajax_test(HttpServletResponse response,Model model) throws Exception {
@@ -149,22 +205,10 @@ public class MainController {
 
 
 
-    //---------------한번에 main2.html-------------------
-    //오늘 전국 베스트 찜
-    @RequestMapping("main")
-    public String main(HttpServletResponse response,Model model) throws Exception {
-        //일별 베스트
-        //맛집
-        List<HashMap<String, Object>> eatjjim = mainService.eatjjim();
-        //카페
-        List<HashMap<String, Object>> cafejjim = mainService.cafejjim();
-        System.out.println("eatjjim"+eatjjim);
-        System.out.println("cafejjim"+cafejjim);
+    //-----------------test2 버튼 만들기-----------------
+    @GetMapping(value = "select3")
+    public String select_ajax_test2(HttpServletResponse response,Model model) throws Exception {
 
-        model.addAttribute("cafejjim",cafejjim);
-        model.addAttribute("eatjjim",eatjjim);
-
-        //월별베스트
         Map<String,List<String>> sigunguMap = mainService.getsidogungu();
         JSONObject obj = new JSONObject();
         response.setContentType("text/html; charset=UTF-8");
@@ -187,19 +231,46 @@ public class MainController {
         model.addAttribute("sigunguMap",sigunguMap);
         System.out.println("sigunguMap_test2"+sigunguMap);
 
-        return "main/main3";
 
+        //String getSido = sidogugunDTO.getSido();
+        //String getGugun = sidogugunDTO.getGugun();
+        //System.out.println("getSido="+getSido);
+        //System.out.println("getGugun="+getGugun);
+        //SidogugunDTO sidogugunDTO = mainService.monthBest();
+
+
+        return "main/select31";
     }
 
-    @GetMapping(value = "sigunguMap3", produces = MediaType.APPLICATION_JSON_VALUE)
+
+    @GetMapping(value = "sigunguMap4", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
-    public Map<String, List<String>> select_ajax3(HttpServletResponse response, Model model) throws Exception {
+    public Map<String, List<String>> select_ajax2_test2(
+            @RequestParam(name = "sido") String sido,
+            @RequestParam(name = "gugun") String gugun,
+            HttpServletResponse response, Model model) throws Exception {
         Map<String, List<String>> sigunguMap = mainService.getsidogungu();
         model.addAttribute("sigunguMap",sigunguMap);
         System.out.println("sigunguMap"+sigunguMap);
+
+        List<HashMap<String, Object>> monthBestList = mainService.monthBest(sido,gugun);
+        System.out.println("monthBestList"+monthBestList);
         return sigunguMap;
     }
 
+    @GetMapping(value = "initialData", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public Map<String, List<String>> getInitialData() throws Exception {
+        Map<String, List<String>> sigunguMap = mainService.getsidogungu();
+        return sigunguMap;
+    }
+
+    @GetMapping("select3?sido={sido}&gugun={gugun}")
+    public String detail(@RequestParam(name = "sido") String sido,
+                         @RequestParam(name = "gugun") String gugun){
+        List<HashMap<String, Object>> monthBestList = mainService.monthBest(sido,gugun);
+        return "main/imsi";
+    }
 
 
 }

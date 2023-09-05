8d0d0e483ed1282af2234e9d630f7ef184816246
min <happysss0505@gmail.com>
2023-09-05 11:19:55 +0900

    9/4 select33/전송한 파라미터 값 콘솔에 찍는 것 까지 완료

--- src/main/java/com/yumpro/ddogo/main/controller/MainController.java
+++ src/main/java/com/yumpro/ddogo/main/controller/MainController.java
@@ -10,7 +10,10 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
@@ -205,7 +208,7 @@ public class MainController {
 
 
 
-    //-----------------test2 버튼 만들기-----------------
+   /* //-----------------test2 버튼 만들기-----------------
     @GetMapping(value = "select3")
     public String select_ajax_test2(HttpServletResponse response,
                                     Model model) throws Exception {
@@ -240,11 +243,11 @@ public class MainController {
         //SidogugunDTO sidogugunDTO = mainService.monthBest();
 
 
-        return "main/select31";
+        return "main/select33";
     }
 
 
- /*   @GetMapping(value = "sigunguMap4", produces = MediaType.APPLICATION_JSON_VALUE)
+ *//*   @GetMapping(value = "sigunguMap4", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     public Map<String, List<String>> select_ajax2_test2(
             @RequestParam(name = "sido") String sido,
@@ -258,7 +261,7 @@ public class MainController {
         model.addAttribute("monthBestList", monthBestList);
         System.out.println("monthBestList"+monthBestList);
         return sigunguMap;
-    }*/
+    }*//*
 
     // 초기 데이터를 가져오는 엔드포인트
     @GetMapping(value = "initialData", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -268,12 +271,134 @@ public class MainController {
         return sigunguMap;
     }
 
-    /*@GetMapping("select3?sido={sido}&gugun={gugun}")
+    *//*@GetMapping("select3?sido={sido}&gugun={gugun}")
     public String detail(@RequestParam(name = "sido") String sido,
                          @RequestParam(name = "gugun") String gugun){
         List<HashMap<String, Object>> monthBestList = mainService.monthBest(sido,gugun);
         return "main/imsi";
-    }*/
+    }*//*
+
+    //-----------------------------------
+ *//*   @PostMapping(value = "select3")
+    public String sidogugunPost(HttpServletResponse response,
+                            @RequestParam(name = "sido") String sido,
+                            @RequestParam(name = "gugun") String gugun,
+                                    Model model) throws Exception {
+
+        Map<String,List<String>> sigunguMap = mainService.getsidogungu();
+        JSONObject obj = new JSONObject();
+        response.setContentType("text/html; charset=UTF-8");
+        PrintWriter out = response.getWriter();
+
+        for(String sidoKey: sigunguMap.keySet() ) { //map의 key의 개수만큼=<=(중복되지않는)sido의 개수만큼
+            List<String> sigunguList = sigunguMap.get(sidoKey);
+            JSONArray jsonArr = new JSONArray(); //배열준비
+            System.out.printf("시도별%s 시군구개수%d \r\n",sidoKey,sigunguList.size());
+            for( String gugun1 :sigunguList) { //sido별  sigungu의 개수만큼
+                jsonArr.put(gugun1);
+            }
+            obj.put(sidoKey,jsonArr);
+        }
+
+        model.addAttribute("sigunguMap",sigunguMap);
+        System.out.println("sigunguMap_test3"+sigunguMap);
+
+        String jsonStr = obj.toString();
+        System.out.println("jsonStr ="+jsonStr); //콘솔출력.확인용
+        out.print(jsonStr); //client로 보내기
+        model.addAttribute(jsonStr);
+
+        List<HashMap<String, Object>> monthBestList=mainService.monthBest(sido,gugun);
+        JSONObject obj2 = new JSONObject();
+        JSONArray monthBestListArr = new JSONArray(); //배열준비
+        for( int i=0; i<monthBestList.size(); i++ ){
+            JSONArray jsonArr2 = new JSONArray(); //배열준비
+            obj2.put("sido",monthBestList.get(1).get(sido));
+            obj2.put("gugun",monthBestList.get(2).get(gugun));
+            monthBestListArr.put(obj2);
+        }
+
+        String jsonStr2 = obj2.toString();
+        System.out.println("jsonStr2 ="+jsonStr2); //콘솔출력.확인용
+        out.print(jsonStr2); //client로 보내기
+
+        return null;
+    }*//*
+
+
+  *//*  @PostMapping(value = "select3")
+    public String sidogugunPost(HttpServletResponse response,
+                            @RequestParam(name = "sido") String sido,
+                            @RequestParam(name = "gugun") String gugun,
+                                    Model model) throws Exception {
+
+        Map<String,List<String>> sigunguMap = mainService.getsidogungu();
+        JSONObject obj = new JSONObject();
+        response.setContentType("text/html; charset=UTF-8");
+        PrintWriter out = response.getWriter();
+
+        for(String sidoKey: sigunguMap.keySet() ) { //map의 key의 개수만큼=<=(중복되지않는)sido의 개수만큼
+            List<String> sigunguList = sigunguMap.get(sidoKey);
+            JSONArray jsonArr = new JSONArray(); //배열준비
+            System.out.printf("시도별%s 시군구개수%d \r\n",sidoKey,sigunguList.size());
+            for( String gugun1 :sigunguList) { //sido별  sigungu의 개수만큼
+                jsonArr.put(gugun1);
+            }
+            obj.put(sidoKey,jsonArr);
+        }
+
+        model.addAttribute("sigunguMap",sigunguMap);
+        System.out.println("sigunguMap_test3"+sigunguMap);
+
+        String jsonStr = obj.toString();
+        System.out.println("jsonStr ="+jsonStr); //콘솔출력.확인용
+        out.print(jsonStr); //client로 보내기
+        model.addAttribute(jsonStr);
+
+        List<HashMap<String, Object>> monthBestList=mainService.monthBest(sido,gugun);
+        JSONObject obj2 = new JSONObject();
+        obj2.put("sido", sido); // sido 추가
+        obj2.put("gugun", gugun); // gugun 추가
+
+        String jsonStr2 = obj.toString();
+        System.out.println("jsonStr2_test =" + jsonStr2); // 콘솔 출력. 확인용
+
+
+      *//**//*  List<HashMap<String, Object>> monthBestList=mainService.monthBest(sido,gugun);
+        JSONObject obj2 = new JSONObject();
+        JSONArray monthBestListArr = new JSONArray(); //배열준비
+        for( int i=0; i<monthBestList.size(); i++ ){
+            JSONArray jsonArr2 = new JSONArray(); //배열준비
+            obj2.put("sido",monthBestList.get(1).get(sido));
+            obj2.put("gugun",monthBestList.get(2).get(gugun));
+            monthBestListArr.put(obj2);
+        }
+
+        String jsonStr2 = obj2.toString();
+        System.out.println("jsonStr2 ="+jsonStr2); //콘솔출력.확인용
+        out.print(jsonStr2); //client로 보내기*//**//*
+
+        return null;
+    }*//*
+
+    @PostMapping(value = "select3")
+    public ResponseEntity<String> sidogugunPost(
+            @RequestParam(name = "sido") String sido,
+            @RequestParam(name = "gugun") String gugun) throws Exception {
+
+        // mainService.monthBest(sido, gugun)을 실행하여 원하는 데이터를 가져옴
+        List<HashMap<String, Object>> monthBestList = mainService.monthBest(sido, gugun);
+
+        // monthBestList를 JSON 형식으로 변환
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonStr = objectMapper.writeValueAsString(monthBestList);
+
+        // JSON 응답 반환
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.setContentType(MediaType.APPLICATION_JSON);
+        return new ResponseEntity<>(jsonStr, responseHeaders, HttpStatus.OK);
+    }
+*/
 
 
 }
---
+++ src/main/java/com/yumpro/ddogo/main/controller/MainController2.java
@@ -0,0 +1,223 @@
+package com.yumpro.ddogo.main.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.yumpro.ddogo.main.service.MainService;
+import jakarta.servlet.http.HttpServletResponse;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@SpringBootApplication
+@Controller
+public class MainController2 {
+    @Autowired
+    private MainService mainService;
+
+
+    //-----------------test2 버튼 만들기-----------------
+    @GetMapping(value = "select3")
+    public String select_ajax_test2(HttpServletResponse response,
+                                    Model model) throws Exception {
+
+        Map<String, List<String>> sigunguMap = mainService.getsidogungu();
+        JSONObject obj = new JSONObject();
+        response.setContentType("text/html; charset=UTF-8");
+        PrintWriter out = response.getWriter();
+
+        for (String sidoKey : sigunguMap.keySet()) { //map의 key의 개수만큼=<=(중복되지않는)sido의 개수만큼
+            List<String> sigunguList = sigunguMap.get(sidoKey);
+            JSONArray jsonArr = new JSONArray(); //배열준비
+            System.out.printf("시도별%s 시군구개수%d \r\n", sidoKey, sigunguList.size());
+            for (String gugun : sigunguList) { //sido별  sigungu의 개수만큼
+                jsonArr.put(gugun);
+            }
+            obj.put(sidoKey, jsonArr);
+        }
+
+        String jsonStr = obj.toString();
+        System.out.println("jsonStr =" + jsonStr); //콘솔출력.확인용
+        //out.print(jsonStr); //client로 보내기
+        model.addAttribute(jsonStr);
+        model.addAttribute("sigunguMap", sigunguMap);
+        System.out.println("sigunguMap_test3" + sigunguMap);
+
+
+        return "main/select34";
+    }
+
+
+    // 초기 데이터를 가져오는 엔드포인트
+    @GetMapping(value = "initialData", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public Map<String, List<String>> getInitialData() throws Exception {
+        Map<String, List<String>> sigunguMap = mainService.getsidogungu();
+        return sigunguMap;
+    }
+
+
+    //-----------------------------------
+    @PostMapping(value = "select3")
+    public ResponseEntity<String> sidogugunPost(HttpServletResponse response,
+                                                @RequestParam(name = "sido") String selectedSido,
+                                                @RequestParam(name = "gugun") String selectedGugun) throws Exception {
+        System.out.println("post진입");
+        System.out.println("selectedSido="+selectedSido);
+        System.out.println("selectedGugun="+selectedGugun);
+        response.setContentType("text/html; charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        // mainService.monthBest(selectedSido, selectedGugun)을 실행하여 원하는 데이터를 가져옴
+        List<HashMap<String, Object>> monthBestList=mainService.monthBest(selectedSido,selectedGugun);
+        JSONObject obj2 = new JSONObject();
+        JSONArray monthBestListArr = new JSONArray(); //배열준비
+        for( int i=0; i<monthBestList.size(); i++ ){
+            JSONArray jsonArr2 = new JSONArray(); //배열준비
+            obj2.put("sido",monthBestList.get(1).get(selectedSido));
+            obj2.put("gugun",monthBestList.get(2).get(selectedGugun));
+            monthBestListArr.put(obj2);
+        }
+
+        String jsonStr2 = obj2.toString();
+        System.out.println("jsonStr2 ="+jsonStr2); //콘솔출력.확인용
+        out.print(jsonStr2); //client로 보내기
+
+        return null;
+    }
+  /*  @PostMapping(value = "select3")
+    public ResponseEntity<String> sidogugunPost(HttpServletResponse response,
+            @RequestParam(name = "sido") String selectedSido,
+            @RequestParam(name = "gugun") String selectedGugun) throws Exception {
+
+        // mainService.monthBest(selectedSido, selectedGugun)을 실행하여 원하는 데이터를 가져옴
+        List<HashMap<String, Object>> monthBestList = mainService.monthBest(selectedSido, selectedGugun);
+
+        // monthBestList를 JSON 형식으로 변환
+        ObjectMapper objectMapper = new ObjectMapper();
+        System.out.println("objectMapper="+objectMapper);
+        String jsonStr_month = objectMapper.writeValueAsString(monthBestList);
+        System.out.println("jsonStr_month" + jsonStr_month);
+
+        // JSON 응답 반환
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.setContentType(MediaType.APPLICATION_JSON);
+        return new ResponseEntity<>(jsonStr_month, responseHeaders, HttpStatus.OK);
+    }*/
+
+
+ /*   @PostMapping(value = "select3")
+    public String sidogugunPost(HttpServletResponse response,
+                            @RequestParam(name = "sido") String selectedSido,
+                            @RequestParam(name = "gugun") String selectedGugun,
+                                    Model model) throws Exception {
+
+        Map<String,List<String>> sigunguMap = mainService.getsidogungu();
+        JSONObject obj = new JSONObject();
+        response.setContentType("text/html; charset=UTF-8");
+        PrintWriter out = response.getWriter();
+
+        for(String sidoKey: sigunguMap.keySet() ) { //map의 key의 개수만큼=<=(중복되지않는)sido의 개수만큼
+            List<String> sigunguList = sigunguMap.get(sidoKey);
+            JSONArray jsonArr = new JSONArray(); //배열준비
+            System.out.printf("시도별%s 시군구개수%d \r\n",sidoKey,sigunguList.size());
+            for( String gugun1 :sigunguList) { //sido별  sigungu의 개수만큼
+                jsonArr.put(gugun1);
+            }
+            obj.put(sidoKey,jsonArr);
+        }
+
+        model.addAttribute("sigunguMap",sigunguMap);
+        System.out.println("sigunguMap_test3"+sigunguMap);
+
+        String jsonStr = obj.toString();
+        System.out.println("jsonStr ="+jsonStr); //콘솔출력.확인용
+        out.print(jsonStr); //client로 보내기
+        model.addAttribute(jsonStr);
+
+        List<HashMap<String, Object>> monthBestList=mainService.monthBest(selectedSido,selectedGugun);
+        JSONObject obj2 = new JSONObject();
+        JSONArray monthBestListArr = new JSONArray(); //배열준비
+        for( int i=0; i<monthBestList.size(); i++ ){
+            JSONArray jsonArr2 = new JSONArray(); //배열준비
+            obj2.put("sido",monthBestList.get(1).get(selectedSido));
+            obj2.put("gugun",monthBestList.get(2).get(selectedGugun));
+            monthBestListArr.put(obj2);
+        }
+
+        String jsonStr2 = obj2.toString();
+        System.out.println("jsonStr2 ="+jsonStr2); //콘솔출력.확인용
+        out.print(jsonStr2); //client로 보내기
+
+        return null;
+    }*/
+
+
+  /*  @PostMapping(value = "select3")
+    public String sidogugunPost(HttpServletResponse response,
+                            @RequestParam(name = "sido") String sido,
+                            @RequestParam(name = "gugun") String gugun,
+                                    Model model) throws Exception {
+
+        Map<String,List<String>> sigunguMap = mainService.getsidogungu();
+        JSONObject obj = new JSONObject();
+        response.setContentType("text/html; charset=UTF-8");
+        PrintWriter out = response.getWriter();
+
+        for(String sidoKey: sigunguMap.keySet() ) { //map의 key의 개수만큼=<=(중복되지않는)sido의 개수만큼
+            List<String> sigunguList = sigunguMap.get(sidoKey);
+            JSONArray jsonArr = new JSONArray(); //배열준비
+            System.out.printf("시도별%s 시군구개수%d \r\n",sidoKey,sigunguList.size());
+            for( String gugun1 :sigunguList) { //sido별  sigungu의 개수만큼
+                jsonArr.put(gugun1);
+            }
+            obj.put(sidoKey,jsonArr);
+        }
+
+        model.addAttribute("sigunguMap",sigunguMap);
+        System.out.println("sigunguMap_test3"+sigunguMap);
+
+        String jsonStr = obj.toString();
+        System.out.println("jsonStr ="+jsonStr); //콘솔출력.확인용
+        out.print(jsonStr); //client로 보내기
+        model.addAttribute(jsonStr);
+
+        List<HashMap<String, Object>> monthBestList=mainService.monthBest(sido,gugun);
+        JSONObject obj2 = new JSONObject();
+        obj2.put("sido", sido); // sido 추가
+        obj2.put("gugun", gugun); // gugun 추가
+
+        String jsonStr2 = obj.toString();
+        System.out.println("jsonStr2_test =" + jsonStr2); // 콘솔 출력. 확인용
+
+
+      *//*  List<HashMap<String, Object>> monthBestList=mainService.monthBest(sido,gugun);
+        JSONObject obj2 = new JSONObject();
+        JSONArray monthBestListArr = new JSONArray(); //배열준비
+        for( int i=0; i<monthBestList.size(); i++ ){
+            JSONArray jsonArr2 = new JSONArray(); //배열준비
+            obj2.put("sido",monthBestList.get(1).get(sido));
+            obj2.put("gugun",monthBestList.get(2).get(gugun));
+            monthBestListArr.put(obj2);
+        }
+
+        String jsonStr2 = obj2.toString();
+        System.out.println("jsonStr2 ="+jsonStr2); //콘솔출력.확인용
+        out.print(jsonStr2); //client로 보내기*//*
+
+        return null;
+    }*/
+
+
+}
+

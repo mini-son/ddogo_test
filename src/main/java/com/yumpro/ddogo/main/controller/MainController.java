8e1e0fa712222999a380b85691f47a70f33f2a48
min <happysss0505@gmail.com>
2023-09-06 21:43:41 +0900

    9/6 마지막커밋

--- src/main/java/com/yumpro/ddogo/main/controller/MainController.java
+++ src/main/java/com/yumpro/ddogo/main/controller/MainController.java
@@ -99,7 +99,7 @@ public class MainController {
         System.out.println("sigunguMap_test3" + sigunguMap);
 
         //return "main/main5";
-        return "main/test18";
+        return "main/test19";
     }
 
     // 초기 데이터를 가져오는 엔드포인트
